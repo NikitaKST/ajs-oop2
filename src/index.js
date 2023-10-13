27c906a5391d8ddb25488da98a8a1ac5137b5a19
NikitaKST <8172535@gmail.com>
2023-10-13 17:39:44 +0500

    class creation completed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // Импортируем JavaScript
-import './js/character';
-import './js/bowerman';
-import './js/daemon';
-import './js/magician';
-import './js/swordsman';
-import './js/undead';
-import './js/zombie';
+import './js/character.js';
+import './js/bowerman.js';
+import './js/daemon.js';
+import './js/magician.js';
+import './js/swordsman.js';
+import './js/undead.js';
+import './js/zombie.js';
--- src/js/magician.js
+++ src/js/magician.js
@@ -1,4 +1,4 @@
-import Character from './characterharacter';
+import Character from './character.js';
 
 export default class Magician extends Character {
   constructor(name) {
