551562048b60fc943d669ffb26733f254ddb28ca
NikitaKST <8172535@gmail.com>
2023-10-13 20:26:22 +0500

    Complete

--- src/js/character.js
+++ src/js/character.js
@@ -15,4 +15,24 @@ export default class Character {
     this.health = 100;
     this.level = 1;
   }
+
+  levelUp() {
+    if (this.health === 0) {
+      throw new Error('Нельзя повысить левел умершего!');
+    }
+    this.level += 1;
+    this.attack *= 1.2;
+    this.defence *= 1.2;
+    this.health = 100;
+    return 1;
+  }
+
+  damage(points) {
+    if (this.health > 0) {
+      this.health -= points * (1 - this.defence / 100);
+    }
+    if (this.health < 0) {
+      this.health = 0;
+    }
+  }
 }
